f838b0681b9c7a08976a3cf48cd1877b957e3cc4
radit <pradityapramana21@gmail.com>
2026-02-20 13:05:24 +0700

    33

--- MahasiswaMain23.java
+++ MahasiswaMain23.java
@@ -1,36 +1,4 @@
 public class MahasiswaMain23 {
-    String nim;
-    String nama;
-    String kelas;
-    double ipk;
-    void tampilkanInformasi() {
-        System.out.println("NIM   : " + nim);
-        System.out.println("Nama  : " + nama);
-        System.out.println("Kelas : " + kelas);
-        System.out.println("IPK   : " + ipk);
-    }
-    void ubahKelas(String kelasBaru) {
-        kelas = kelasBaru;
-    }
-    void updateIpk(double ipkBaru) {
-        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
-            ipk = ipkBaru;
-            System.out.println("IPK berhasil diperbarui menjadi: " + ipk);
-        } else {
-            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
-        }
-    }
-    String nilaiKinerja() {
-        if (ipk >= 3.5) {
-            return "Kinerja sangat baik";
-        } else if (ipk >= 3.0) {
-            return "Kinerja baik";
-        } else if (ipk >= 2.0) {
-            return "Kinerja cukup";
-        } else {
-            return "Kinerja kurang";
-        }
-    }
     public static void main(String[] args) {
         Mahasiswa23 mhs1 = new Mahasiswa23();
         mhs1.nama = "Muhammad Ali Farhan";
@@ -41,9 +9,47 @@ public class MahasiswaMain23 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
-Mahasiswa23 mhs2 = new Mahasiswa23("Annisa Nabila", "2141720160", 3.25, "TI 2L");
-mhs2.updateIpk(3.30);
-mhs2.tampilkanInformasi();
+        Mahasiswa23 mhs2 = new Mahasiswa23("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
         System.out.println("Evaluasi Kinerja: " + mhs1.nilaiKinerja());
+    }
+}
+class Mahasiswa23 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+    public Mahasiswa23() {
+    }
+    public Mahasiswa23(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+    void tampilkanInformasi() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("----------------------");
+    }
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+            System.out.println("IPK berhasil diperbarui menjadi: " + ipk);
+        } else {
+            System.out.println("IPK tidak valid.");
+        }
+    }
+    String nilaiKinerja() {
+        if (ipk >= 3.5) return "Kinerja sangat baik";
+        else if (ipk >= 3.0) return "Kinerja baik";
+        else if (ipk >= 2.0) return "Kinerja cukup";
+        else return "Kinerja kurang";
     }
 }
