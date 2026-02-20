55ee0a47b00bb991f49c6a192a9c7fdd85838931
radit <pradityapramana21@gmail.com>
2026-02-20 12:48:35 +0700

    2

---
+++ MahasiswaMain23.java
@@ -0,0 +1,46 @@
+public class MahasiswaMain23 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+    void tampilkanInformasi() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+    }
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+            System.out.println("IPK berhasil diperbarui menjadi: " + ipk);
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+    public static void main(String[] args) {
+        Mahasiswa23 mhs1 = new Mahasiswa23();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+        System.out.println("Evaluasi Kinerja: " + mhs1.nilaiKinerja());
+    }
+}
